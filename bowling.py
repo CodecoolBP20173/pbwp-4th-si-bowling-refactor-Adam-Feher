1df78e358ed2b6623c8a84e21be0301761f273b6
Adam Feher <fhradm@gmail.com>
2017-12-03 19:11:06 +0100

    get_value more simplified

--- bowling.py
+++ bowling.py
@@ -1,45 +1,48 @@
+STRIKE = "x"
+SPARE = "/"
+MISS = '-'
+
 def score(game):
-    return change_result(game)
+    result = 0
+    frame = 1
+    in_first_half = True
+    return change_score(game, frame, in_first_half, result)
 
-
-def change_result(game, frame=1, in_first_half=True, result=0):
-    STRIKE = "x"
-    SPARE = "/"
+def change_score(game, frame, in_first_half, result):
     for i in range(len(game)):
-        if game[i] == SPARE:
+        if game[i] == '/':
             result += 10 - last
         else:
             result += get_value(game[i])
-        if frame < 10:
-            if game[i] == SPARE:
+        if frame < 10 and get_value(game[i]) == 10:
+            if game[i] == '/':
                 result += get_value(game[i + 1])
-            elif game[i].lower() == STRIKE:
+            elif game[i] == 'X' or game[i] == 'x':
                 result += get_value(game[i + 1])
-                if game[i + 2] == SPARE:
+                if game[i + 2] == '/':
                     result += 10 - get_value(game[i + 1])
                 else:
                     result += get_value(game[i + 2])
         last = get_value(game[i])
-        if not in_first_half:
-            frame += 1
+
+        if not in_first_half or game[i].lower() == STRIKE:
             in_first_half = True
-        if in_first_half:
+            frame += 1
+        else:
             in_first_half = False
-        if game[i].lower() == STRIKE:
-            in_first_half = True
-            frame += 1
     return result
 
 
 def get_value(char):
-    if char in ("1", "2", "3", "4", "5", "6", "7", "8", "9"):
-        return int(char)
-    elif char.lower() in ("x", "/"):
-        return 10
-    elif char == '-':
-        return 0
-    else:
-        raise ValueError()
+    try:
+        char = int(char)
+        if 1 <= char and char <= 9:
+            return char
 
-
-score("1/35XXX458/X3/23")
+    except ValueError:
+        if char.lower() in (STRIKE, SPARE):
+            return 10
+        elif char.lower() == MISS:
+            return 0
+        else:
+            raise ValueError()
